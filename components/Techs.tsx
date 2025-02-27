26364c62fc8653fd718ce3dfb5b86b869cc5e8b3
Kareemknj <kareem_kj@hotmail.com>
2025-02-27 21:42:27 +0100

    Fix TypeScript configuration warnings

--- components/Techs.tsx
+++ components/Techs.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Techs = () => {
   return (
     <div className="flex justify-center flex-wrap item-center gap-8 md:gap-28 max-lg:mt-10 py-20">
-        {companies.map(({id,img,name,nameImg}) => (
+        {companies.map(({id,img,name}) => (
             <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
                 <img
                 src={img}
